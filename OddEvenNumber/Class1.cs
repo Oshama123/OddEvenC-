cdd9343c64bf6f6d6ba7e876fcadc97b13db4588
Oshama123 <laodeoshama0001@gmail.com>
2022-10-13 09:26:15 +0700

    initial commit

---
+++ OddEvenNumber/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddEvenNumber
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int Solut;
+
+            Console.WriteLine("Insert Number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            Solut = num % 2;
+
+            if (Solut == 1)
+            {
+                Console.WriteLine("Result = " + num.ToString() + " was Odd Number");
+            }
+            else
+            {
+                Console.WriteLine("Result = " + num.ToString() + " was Even Number");
+            }
+            Console.WriteLine("");
+            Console.ReadKey();
+        }
+    }
+}
